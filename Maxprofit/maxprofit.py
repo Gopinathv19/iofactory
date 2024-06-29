47fa756b6f5492636696a3a9f5663852186600d8
Gopinathv19 <142018751+Gopinathv19@users.noreply.github.com>
2024-06-29 13:16:49 +0530

    Update maxprofit.py

--- Maxprofit/maxprofit.py
+++ Maxprofit/maxprofit.py
@@ -1,7 +1,7 @@
 list1 = [0] * 3
 time = [5, 4, 10]
 cost = [1500, 1000, 3000]
-inputTime = int(input("Enter the available time: "))
+inputTime = int(input("Enter the  time: "))
 king = []  
 ans = []  
 def findMax(ind, currentTime, profit, l):
